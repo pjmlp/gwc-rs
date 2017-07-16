ef495e136efc35aff98ff9965a0ca73a62fc43a0
pjmlp <pjmlp@progtools.org>
2017-07-16 15:06:54 +0200

    Managed to make everything work properly with the old struct design

--- src/main.rs
+++ src/main.rs
@@ -27,133 +27,209 @@ use std::path::PathBuf;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::rc::{Rc};
-use std::cell::{RefCell};
+use std::rc::Rc;
 
-///  Called when the user selects the
-/// File->Open option
-fn on_menu_open(win : &Window, lbl: &Rc<RefCell<Label>>) {
-    let filesel = FileChooserDialog::new(Some("Choose a file"), Some(win),
-                                                FileChooserAction::Open);
-    filesel.add_buttons(&[
-        ("Open", ResponseType::Ok.into()),
-        ("Cancel", ResponseType::Cancel.into())
-    ]);
+/// The application window state
+#[derive(Debug, Clone)]
+struct GWCApp {
+    /// the window label used to display the counters
+    msg_label: Option<Rc<Label>>,
 
-    filesel.set_select_multiple(true);
-    filesel.run();
-    let file = filesel.get_filename();
-    filesel.destroy();
+    /// a kind of handle for the Gtk+ window
+    window : Option<Rc<Window>>
 
-    if let Some(filename) = file {
-        process_file(filename, &win, &lbl);
-    }
 }
 
+impl GWCApp {
+
+    /// Provides a new instance of the GWC application
+    pub fn new() -> GWCApp {
+        GWCApp { msg_label: None, window: None }
+    }
+
+    /// Responsible for initializing the application state, including the whole UI
+    pub fn init(&mut self) {
+        let v_box = gtk::Box::new(gtk::Orientation::Vertical, 10);
+
+        let win = Window::new(WindowType::Toplevel);
+        win.set_title("GWC");
+        win.set_position(gtk::WindowPosition::Center);
+        win.set_size_request(400, 400);
+
+        win.connect_delete_event(|_, _| {
+            gtk::main_quit();
+            Inhibit(false)
+        });
+
+        // We will need this in helper methods
+        let file_counter = Label::new("No file counted");
+
+        // The fields must be updated for the helper methods to work as expected.
+        self.window = Some(Rc::new(win));
+        self.msg_label = Some(Rc::new(file_counter));
+
+        // create the application menu
+        let menu_bar = self.init_menus();
+        v_box.pack_start(&menu_bar, false, false, 0);
+
+        // followed by the toolbar
+        let tool_bar = self.init_toolbar();
+        v_box.pack_start(&tool_bar, false, false, 0);
+
+        // Create the text label for showing the word count
+        
+        if let Some (ref lbl) = self.msg_label {
+            v_box.pack_start(lbl.as_ref(), true, true, 0);
+        }
+        
+        if let Some (ref w) = self.window {
+            w.add(&v_box);
+        }
 
 
-///  Called when the user presses the
-/// Ok button on the FileSelection dialog
-fn process_file (filename : PathBuf, win : &Window, lbl: &Rc<RefCell<Label>>) {
-    println!("Files: {:?}", filename);
+    }
 
-    if let Some ((words, lines, bytes)) = count_words(&filename) {
-        let msg = format!("The file {:?}, has {} lines, {} words and {} bytes", filename, words, lines, bytes);
-            lbl.borrow().set_text(&msg);
-    } else {
+    /// Displays the application window
+    pub fn show(&self) {
+        if let Some(ref win) = self.window {
+            win.show_all()
+        } else {
+            panic!("Window has not been properly initialized");
+        }
+    }
+
+    ///  Called when the user selects the
+    /// File->Open option
+    fn on_menu_open(win:&Rc<Window>, lbl : &Rc<Label>) {
+        let filesel = FileChooserDialog::new(Some("Choose a file"), Some(win.as_ref()),
+                                                    FileChooserAction::Open);
+        filesel.add_buttons(&[
+            ("Open", ResponseType::Ok.into()),
+            ("Cancel", ResponseType::Cancel.into())
+        ]);
+
+        filesel.set_select_multiple(true);
+        filesel.run();
+        let file = filesel.get_filename();
+        filesel.destroy();
+
+        if let Some(filename) = file {
+            GWCApp::process_file(filename, &win, &lbl);
+        }
+    }
+
+
+
+    ///  Called when the user presses the
+    /// Ok button on the FileSelection dialog
+    fn process_file (filename : PathBuf, win:&Rc<Window>, lbl : &Rc<Label>) {
+        if let Some ((words, lines, bytes)) = count_words(&filename) {
+            let msg = format!("The file {:?}, has {} lines, {} words and {} bytes", filename, words, lines, bytes);
+            lbl.set_text(&msg.to_string());
+        } else {
             let msg = format!("Could not open file {:?}", filename);
-            let dialog = MessageDialog::new(Some(win), DIALOG_MODAL,
+            let dialog = MessageDialog::new(Some(win.as_ref()), DIALOG_MODAL,
                 MessageType::Error, ButtonsType::Ok, &msg);
             dialog.run();
             dialog.destroy();
+        }
     }
+
+    /// Creates the application menus
+    fn init_menus (&self) -> MenuBar {
+        let menu = Menu::new();
+        let menu_bar = MenuBar::new();
+        let file = MenuItem::new_with_label("File");
+    
+        let quit = MenuItem::new_with_label("Quit");
+        let file_item = MenuItem::new();
+        let file_box = gtk::Box::new(gtk::Orientation::Horizontal, 0);
+        let file_image = Image::new_from_icon_name("document-open", IconSize::Menu.into());
+        let file_label = Label::new(Some("File"));
+
+        file_box.pack_start(&file_image, false, false, 0);
+        file_box.pack_start(&file_label, true, true, 0);
+        file_item.add(&file_box);
+
+        menu.append(&file_item);
+        menu.append(&quit);
+        file.set_submenu(Some(&menu));
+        menu_bar.append(&file);
+
+        // Extras menu
+        let extras_menu = Menu::new();
+        let extras = MenuItem::new_with_label("Extras");
+        let about = MenuItem::new_with_label("About");
+
+        extras_menu.append(&about);
+        extras.set_submenu(Some(&extras_menu));
+        menu_bar.append(&extras);
+
+        if let Some (ref label) = self.msg_label {
+            if let Some (ref win) = self.window {
+                let w = win.to_owned().clone();
+                let l = label.clone();
+
+                file_item.connect_activate(move |_| {
+                    GWCApp::on_menu_open(&w, &l);
+                });
+            }
+        }
+
+        quit.connect_activate(|_| {
+            gtk::main_quit();
+        });
+
+        about.connect_activate(move |_| {
+            let p = AboutDialog::new();
+            p.set_authors(&["Paulo Pinto"]);
+            p.set_website_label(Some("A simple GUI version of wc"));
+            p.set_website(Some("https://github.com/pjmlp/gwc-rs"));
+            p.set_authors(&["Paulo Pinto"]);
+            p.set_title("About GWC");
+            p.set_comments("A port of the original C++/Gnomemm application into Rust/Gtk-rs.");
+            p.run();
+            p.destroy();
+        });
+
+        menu_bar
+    }
+
+    /// Creates the application toolbar
+    fn init_toolbar(&self) -> Toolbar {
+        let toolbar = Toolbar::new();
+        toolbar.set_style(ToolbarStyle::Both);
+
+        let open_btn_image = Image::new_from_icon_name("document-open", IconSize::LargeToolbar.into());
+        let open_btn = ToolButton::new(Some(&open_btn_image), "Open");
+        toolbar.insert(&open_btn, 0);
+
+        let sep = SeparatorToolItem::new();
+        toolbar.insert(&sep, 1);
+
+        let quit_btn_image = Image::new_from_icon_name("application-exit", IconSize::LargeToolbar.into());
+        let quit_btn = ToolButton::new(Some(&quit_btn_image), "Quit");
+        toolbar.insert(&quit_btn, 2);
+
+
+        if let Some (ref label) = self.msg_label {
+            if let Some (ref win) = self.window {
+                let w = win.to_owned().clone();
+                let l = label.clone();
+
+                open_btn.connect_clicked(move |_| {
+                    GWCApp::on_menu_open(&w, &l);
+                });
+            }
+        }
+
+        quit_btn.connect_clicked(|_| {
+            gtk::main_quit();
+        });
+
+        toolbar
+    } 
 }
-
-/// Creates the application menus
-fn init_menus (window : &Window, msg: &Rc<RefCell<Label>>) -> MenuBar {
-    let menu = Menu::new();
-    let menu_bar = MenuBar::new();
-    let file = MenuItem::new_with_label("File");
-
-    let quit = MenuItem::new_with_label("Quit");
-    let file_item = MenuItem::new();
-    let file_box = gtk::Box::new(gtk::Orientation::Horizontal, 0);
-    let file_image = Image::new_from_icon_name("document-open", IconSize::Menu.into());
-    let file_label = Label::new(Some("File"));
-
-    file_box.pack_start(&file_image, false, false, 0);
-    file_box.pack_start(&file_label, true, true, 0);
-    file_item.add(&file_box);
-
-    menu.append(&file_item);
-    menu.append(&quit);
-    file.set_submenu(Some(&menu));
-    menu_bar.append(&file);
-
-    // Extras menu
-    let extras_menu = Menu::new();
-    let extras = MenuItem::new_with_label("Extras");
-    let about = MenuItem::new_with_label("About");
-
-    extras_menu.append(&about);
-    extras.set_submenu(Some(&extras_menu));
-    menu_bar.append(&extras);
-
-    let win = window.clone();
-    let lbl = msg.clone();
-    file_item.connect_activate(move |_| {
-        on_menu_open(&win, &lbl);
-    });
-
-
-    quit.connect_activate(|_| {
-        gtk::main_quit();
-    });
-
-    about.connect_activate(move |_| {
-        let p = AboutDialog::new();
-        p.set_authors(&["Paulo Pinto"]);
-        p.set_website_label(Some("A simple GUI version of wc"));
-        p.set_website(Some("https://github.com/pjmlp/gwc-rs"));
-        p.set_authors(&["Paulo Pinto"]);
-        p.set_title("About GWC");
-        p.set_comments("A port of the original C++/Gnomemm application into Rust/Gtk-rs.");
-        p.run();
-        p.destroy();
-    });
-
-    menu_bar
-}
-
-/// Creates the application toolbar
-fn init_toolbar(window : &Window, msg: &Rc<RefCell<Label>>) -> Toolbar {
-    let toolbar = Toolbar::new();
-    toolbar.set_style(ToolbarStyle::Both);
-
-    let open_btn_image = Image::new_from_icon_name("document-open", IconSize::LargeToolbar.into());
-    let open_btn = ToolButton::new(Some(&open_btn_image), "Open");
-    toolbar.insert(&open_btn, 0);
-
-    let sep = SeparatorToolItem::new();
-    toolbar.insert(&sep, 1);
-
-    let quit_btn_image = Image::new_from_icon_name("application-exit", IconSize::LargeToolbar.into());
-    let quit_btn = ToolButton::new(Some(&quit_btn_image), "Quit");
-    toolbar.insert(&quit_btn, 2);
-
-    let win = window.clone();
-    let lbl = msg.clone();
-    open_btn.connect_clicked(move |_| {
-        on_menu_open(&win, &lbl);
-    });
-
-    quit_btn.connect_clicked(|_| {
-        gtk::main_quit();
-    });
-
-    toolbar
-} 
-
 
 /// Counts the number of words, lines and bytes on the given file
 fn count_words(filename: &PathBuf) -> Option<(usize, usize, usize)> {
@@ -188,38 +264,10 @@ fn main() {
         return;
     }
 
-    let v_box = gtk::Box::new(gtk::Orientation::Vertical, 10);
+    let mut window = GWCApp::new();
 
-
-    let window = Window::new(WindowType::Toplevel);
-    window.set_title("GWC");
-    window.set_position(gtk::WindowPosition::Center);
-    window.set_size_request(400, 400);
-
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
-
-
-    // Create the text label for showing the word count
-    let file_counter = Rc::new(RefCell::new(Label::new("No file counted")));
-
-    // create the application menu
-    let menu_bar = init_menus(&window, &file_counter);
-    v_box.pack_start(&menu_bar, false, false, 0);
-
-    // followed by the toolbar
-    let tool_bar = init_toolbar(&window, &file_counter);
-    v_box.pack_start(&tool_bar, false, false, 0);
-
-    // Create the text label for showing the word count
-    let file_counter = Label::new("No file counted");
-    v_box.pack_start(&file_counter, true, true, 0);
-
-    window.add(&v_box);
-
-    window.show_all();
+    window.init();
+    window.show();
 
     gtk::main();
 }
