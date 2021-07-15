96663a0e05e584522ca09dcae3b8cb709f4a3bdf
pjmlp <pjmlp@progtools.org>
2021-07-15 22:56:21 +0200

    gen-docs.sh is no longer need, issue 1520 has been fixed for quite some time

--- gen-docs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Generates the Rust documentation taking into account the private datatypes.
-# Taken from https://github.com/rust-lang/cargo/issues/1520
-cargo rustdoc --open -- --no-defaults --passes collapse-docs --passes unindent
